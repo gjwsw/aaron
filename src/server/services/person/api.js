d29b1ff404dc8b709453ae479351bfa2b146c338
Dominik Schreiber <domischreib@gmail.com>
2015-10-02 17:33:23 +0200

    add GET /:id route

--- src/server/services/person/api.js
+++ src/server/services/person/api.js
@@ -5,35 +5,47 @@ import {Router} from 'express';
 export const router = new Router();
 
 router.route('/')
-   .get((req, res) => {
-       req.models.person.find((err, result) => {
-           if (err) {
-               res.status(500)
-                  .json(err);
-           } else {
-               res.json(result.map(person => `${req.baseUrl}/${person.id}`));
-           }
-       });
-   })
-   .post((req, res) => {
-       req.models.person.create(req.body, (err, result) => {
-           if (err) {
-               res.status(400)
-                  .json(err);
-           } else {
-               res.json(`${req.baseUrl}/${result.id}`);
-           }
-       });
-   })
-   .delete((req, res) => {
-       req.models.person.count({}, (err, n) => {
-           req.models.person.find().remove(err => {
-               if (err) {
-                   res.status(500)
-                      .json(err);
-               } else {
-                   res.json(`successfully deleted ${n} person${n === 1 ? '' : 's'}`);
-               }
-           });
-       });
-   });
+      .get((req, res) => {
+          req.models.person.find((err, result) => {
+              if (err) {
+                  res.status(500)
+                     .json(err);
+              } else {
+                  res.json(result.map(person => `${req.baseUrl}/${person.id}`));
+              }
+          });
+      })
+      .post((req, res) => {
+          req.models.person.create(req.body, (err, result) => {
+              if (err) {
+                  res.status(400)
+                     .json(err);
+              } else {
+                  res.json(`${req.baseUrl}/${result.id}`);
+              }
+          });
+      })
+      .delete((req, res) => {
+          req.models.person.count({}, (err, n) => {
+              req.models.person.find().remove(err => {
+                  if (err) {
+                      res.status(500)
+                         .json(err);
+                  } else {
+                      res.json(`successfully deleted ${n} person${n === 1 ? '' : 's'}`);
+                  }
+              });
+          });
+      });
+
+router.route('/:id')
+      .get((req, res) => {
+          req.models.person.get(req.params.id, (err, result) => {
+              if (err) {
+                  res.status(400)
+                     .json(err);
+              } else {
+                  res.json(result);
+              }
+          });
+      });
--- src/server/services/person/api.spec.js
+++ src/server/services/person/api.spec.js
@@ -18,39 +18,64 @@ describe('person', () => {
             .get('/')
             .set('Accept', 'application/json')
             .expect(200)
-            .end((err, res) => {
+            .end((err, result) => {
                 if (err) {
                     throw err;
                 }
-                assert.ok(_.isArray(JSON.parse(res.text)));
+                assert.ok(_.isArray(JSON.parse(result.text)));
                 done();
             });
     });
+
     it(`${POST} / should create a new person`, done => {
         request(app)
             .post('/')
-            .send({givenname: 'Hugo', familyname: 'Kraut', age: 77})
+            .send({givenname: 'foo', familyname: 'bar', age: 1})
             .set('Accept', 'application/json')
             .expect(200)
-            .end((err, res) => {
+            .end((err, result) => {
                 if (err) {
                     throw err;
                 }
-                assert.ok(/\/\d+"$/.test(res.text));
+                assert.ok(/\/\d+"$/.test(result.text));
                 done();
             });
     });
+    
     it(`${DELETE} / should bulk delete all persons`, done => {
         request(app)
             .delete('/')
             .set('Accept', 'application/json')
             .expect(200)
-            .end((err, res) => {
+            .end((err, result) => {
                 if (err) {
                     throw err;
                 }
-                assert.ok(/successfully deleted \d+/.test(res.text));
+                assert.ok(/successfully deleted \d+/.test(result.text));
                 done();
+            });
+    });
+
+    it(`${GET} /:id should retrieve a single item`, done => {
+        const expected = {givenname: 'foo', familyname: 'bar', age: 2};
+        request(app)
+            .post('/')
+            .send(expected)
+            .expect(200)
+            .end((err, url) => {
+                if (err) {
+                    throw err;
+                }
+                request(app)
+                    .get(url.body)
+                    .expect(200)
+                    .end((err, result) => {
+                        if (err) {
+                            throw err;
+                        }
+                        Object.keys(expected).forEach(key => assert.deepEqual(expected[key], result.body[key]));
+                        done();
+                    });
             });
     });
 });
