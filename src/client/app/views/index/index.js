652e0308a33e79f79b5399b7b12fe4e4307b198a
Dominik Schreiber <domischreib@gmail.com>
2015-10-03 09:01:22 +0200

    add "dashboard" index view

--- src/client/app/views/index/index.js
+++ src/client/app/views/index/index.js
@@ -3,21 +3,18 @@
 'use strict';
 
 angular.module('aaron.view.index', [
-    'ngRoute'
+    'aaron.service.person'
+  , 'ngRoute'
   , 'ng'
 ])
 
 .config(($routeProvider) => {
     $routeProvider.when('/', {
         controller: 'IndexController',
-        templateUrl: 'app/views/index/index.tpl.html'
+        templateUrl: '/app/views/index/index.tpl.html'
     });
 })
 
-.controller('IndexController', ($scope, $http, $q) => {
-    $http.get('/api/v1/person').success(urls => {
-        $q.all(urls.map($http.get)).then(results => {
-            $scope.persons = results.map(result => result.data);
-        });
-    });
+.controller('IndexController', ($scope, Person) => {
+    $scope.persons = Person.query();
 });
